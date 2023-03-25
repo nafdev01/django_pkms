27f928aca6f87984f934299ae0c2bd7ceef316b8
nmatoya <naftaldev@gmail.com>
2023-03-25 21:55:38 +0300

    updated ico

--- django_pkms/settings.py
+++ django_pkms/settings.py
@@ -140,7 +140,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # media files
 MEDIA_URL = "media/"
 MEDIAFILES_DIRS = [os.path.join(BASE_DIR, "media")]
-STATIC_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
