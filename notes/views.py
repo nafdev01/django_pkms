29a08c290027bdac45fd12ca31f202c24093fc00
nmatoya <naftaldev@gmail.com>
2023-03-27 16:49:18 +0300

    updated dashboard

--- notes/views.py
+++ notes/views.py
@@ -22,7 +22,7 @@ def dashboard(request):
     courses = Course.objects.filter(student_id=student.id).order_by("updated")
     latest_entries = Entry.objects.filter(
         subtopic__topic__course__student_id=student.id
-    ).order_by("-updated")[:10]
+    ).order_by("-updated")[:7]
 
     today = timezone.now().date()
     objectives = Objective.objects.filter(
