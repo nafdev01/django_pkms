2dee4132d62cf59e9ba587215cb6eaf4474f46e2
nafdev01 <naftaldev@gmail.com>
2023-06-19 16:08:04 +0300

    ..

--- notes/forms.py
+++ notes/forms.py
@@ -2,7 +2,6 @@
 from django.utils.translation import gettext_lazy as _
 from django import forms
 from notes.models import *
-from markdownx.fields import MarkdownxWidget
 
 class CourseForm(forms.ModelForm):
     class Meta:
@@ -26,7 +25,3 @@ class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ["name","content","revised"]
-        widgets = {
-            "content": MarkdownxWidget(attrs={"cols": 80, "rows": 20}),
-        }
-        
