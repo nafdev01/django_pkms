dba6abede7681d5e72e8e7e05c433b010c223e73
nafdev01 <naftaldev@gmail.com>
2023-06-19 14:16:36 +0300

    ...

--- django_pkms/settings.py
+++ django_pkms/settings.py
@@ -136,8 +136,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-if DEVELOPMENT_MODE is not True:
-    from .cdn.conf import *  # noqa
+from .cdn.conf import *  # noqa
 
 # media files
 MEDIA_URL = "/media/"
